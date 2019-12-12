43fcffe09e7cca15d242051e7980e58d9c203c6b
sunlightcode <opensmalldb@qq.com>
2019-12-12 15:15:49 +0800

    sunlightcode update

--- sunlightcoin/contrib/spendfrom/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='STCspendfrom',
-      version='1.0',
-      description='Command-line utility for sunlightcoin "coin control"',
-      author='Gavin Andresen',
-      author_email='gavin@sunlightcoinfoundation.org',
-      requires=['jsonrpc'],
-      scripts=['spendfrom.py'],
-      )
--- sunlightcoin/share/qt/make_windows_icon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/sunlightcoin.png
-ICON_DST=../../src/qt/res/icons/sunlightcoin.ico
-convert ${ICON_SRC} -resize 16x16 sunlightcoin-16.png
-convert ${ICON_SRC} -resize 32x32 sunlightcoin-32.png
-convert ${ICON_SRC} -resize 48x48 sunlightcoin-48.png
-convert sunlightcoin-16.png sunlightcoin-32.png sunlightcoin-48.png ${ICON_DST}
-
